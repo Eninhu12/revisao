c53cc7e2f7239c31019794963a607437a96f6164
Eninhu12 <87648902+Eninhu12@users.noreply.github.com>
2021-07-22 11:42:45 -0300

    Add files via upload

--- Programa2.java
+++ Programa2.java
@@ -2,13 +2,16 @@ package br.com.enio.revisao;
 
 public class Programa2 {
     public static void main(String[] args_) {
-        String[] args= {"dividir","4","2"};
+        String[] args= {"multiplicar","4","2","batata"};
         
         int r;
         Integer a = Integer.valueOf(args[1]);
         Integer b = Integer.valueOf(args[2]);
+        if(args.length>3) {
+        	System.out.println("ERROERROERROERRO!!!!!");
+        }
         
-        if("dividir".equals(args[0])){
+        else if("dividir".equals(args[0])){
             r=a/b;
             System.out.println("O resultdo da divisão é : "+r);
         }
